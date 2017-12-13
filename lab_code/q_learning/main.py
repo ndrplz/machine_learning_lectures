42a08877d6a673c0e680c9839ea96f4ea5fa318a
ndrplz <ndrplz@gmail.com>
2017-12-13 13:05:17 +0100

    Add arg parsing and python2-3 compatibility

--- lab_code/q_learning/main.py
+++ lab_code/q_learning/main.py
@@ -1,22 +1,41 @@
+import os
 import time
+import argparse
 from environment import Environment
 from agent import Agent
 
 
+# For python2 compatibility (notice: python2 support will be removed in the future)
+try:
+    input = raw_input
+except NameError:
+    pass
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n_episodes', type=int, default=500, help='Number of training episodes', metavar='')
+    parser.add_argument('--n_rows', type=int, default=13, help='Number of rows in the labyrinth', metavar='')
+    parser.add_argument('--n_cols', type=int, default=33, help='Number of cols in the labyrinth', metavar='')
+    parser.add_argument('--n_walls', type=int, default=100, help='Number of walls in the labyrinth', metavar='')
+    parser.add_argument('--epsilon', type=float, default=0.01, help='Agent epsilon for eps-greedy policy', metavar='')
+    parser.add_argument('--alpha', type=float, default=1.0, help='Agent alpha for step size', metavar='')
+    parser.add_argument('--gamma', type=float, default=1.0, help='Return discount factor', metavar='')
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
 
-    n_episodes = 1000
-    n_rows, n_cols = 13, 33
-    n_walls = 150
+    # Parse command line arguments
+    args = parse_arguments()
 
-    environment = Environment(n_rows, n_cols, n_walls)
+    environment = Environment(n_rows=args.n_rows, n_cols=args.n_cols, n_walls=args.n_walls)
 
-    agent = Agent(n_rows, n_cols)
+    agent = Agent(n_rows=args.n_rows, n_cols=args.n_cols, epsilon=args.epsilon, alpha=args.alpha, gamma=args.gamma)
 
-    for e in range(n_episodes):
-        print(environment.policy_str(agent))
+    for e in range(args.n_episodes):
 
-        episode_return = 0
+        cumulative_reward = 0
         state = environment.start_new_episode()
 
         while True:
@@ -24,7 +43,7 @@ if __name__ == '__main__':
             action = agent.get_action_eps_greedy(*state)
             next_state, reward, is_over = environment.perform_action(action)
 
-            episode_return += reward  # todo add discount
+            cumulative_reward += reward
 
             agent.update_Q(old_state=state,
                            action=action,
@@ -36,15 +55,24 @@ if __name__ == '__main__':
             if is_over:
                 break
 
-        print('Episode: {:03d} - Return this episode: {}'.format(e, episode_return))
+        print('Episode: {:03d} - Cumulative reward this episode: {}'.format(e, cumulative_reward))
 
-    # Test
+    input('End of training. \n\nPress `ENTER` to start testing.')
+
     state = environment.start_new_episode()
     while True:
+
+        os.system('cls' if os.name == 'nt' else 'clear')  # clear screen
+        print('Learnt policy:')
+        print(environment.policy_str(agent))
+        print('Testing policy:')
         print(environment)
+
         action = agent.get_action_greedy(*state)
         next_state, reward, is_over = environment.perform_action(action)
         state = next_state
+
         if is_over:
             break
+
         time.sleep(0.5)
